38dcfd2faa8963a2d26b7c749bf3ec594d5b2a8b
Deyson Venté <deiandresvent@hotmail.com>
2024-05-10 15:58:56 -0500

    ruteo configurado e implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'characters',
+    loadChildren: () =>
+      import('./characters/characters.module').then((m) => m.CharactersModule),
+  },
+  {
+    path: 'episodes',
+    loadChildren: () =>
+      import('./episodes/episodes.module').then((m) => m.EpisodesModule),
+  },
+  {
+    path: 'locations',
+    loadChildren: () =>
+      import('./locations/locations.module').then((m) => m.LocationsModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'characters',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
