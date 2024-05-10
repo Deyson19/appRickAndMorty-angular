fbfd1b1d0835d9fbeac7b09a6381634b96234c7e
Deyson Venté <deiandresvent@hotmail.com>
2024-05-10 16:57:19 -0500

    characters layout creado, recibe la ruta especificada en el approuting

---
+++ src/app/characters/layout/characters-layout/characters-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './characters-layout.component.html',
+  styleUrls: ['./characters-layout.component.css']
+})
+export class CharactersLayoutComponent {
+
+}
