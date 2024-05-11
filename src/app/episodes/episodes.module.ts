932b28d9e3d632330ce2fc11515069130e1dc261
Deyson Venté <deiandresvent@hotmail.com>
2024-05-11 16:08:03 -0500

    card creado y diseñado

--- src/app/episodes/episodes.module.ts
+++ src/app/episodes/episodes.module.ts
@@ -6,9 +6,10 @@ import { RouterModule } from '@angular/router';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { SharedModule } from '../shared/shared/shared.module';
 import { EpisodePageComponent } from './pages/episode-page/episode-page.component';
+import { EpisodeCardComponent } from './components/episode-card/episode-card.component';
 
 @NgModule({
-  declarations: [ListPageComponent, EpisodePageComponent],
+  declarations: [ListPageComponent, EpisodePageComponent, EpisodeCardComponent],
   imports: [CommonModule, EpisodesRoutingModule, SharedModule],
 })
 export class EpisodesModule {}
