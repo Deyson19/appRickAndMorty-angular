7b1719827d7cbd10a6805876ea6ec00d0ed88bb0
Deyson Venté <deiandresvent@hotmail.com>
2024-05-10 19:14:06 -0500

    renderizar el componente de acuerdo a la ruta

--- src/app/characters/layout/characters-layout/characters-layout.component.ts
+++ src/app/characters/layout/characters-layout/characters-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './characters-layout.component.html',
-  styleUrls: ['./characters-layout.component.css']
+  styleUrls: ['./characters-layout.component.css'],
 })
-export class CharactersLayoutComponent {
-
-}
+export class CharactersLayoutComponent {}
