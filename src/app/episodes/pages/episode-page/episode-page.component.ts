f3c61c6d05539bb1ee51fb04e034808ca322751d
Deyson Venté <deiandresvent@hotmail.com>
2024-05-11 15:03:10 -0500

    Pagina para ver los detalles del episodio

---
+++ src/app/episodes/pages/episode-page/episode-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './episode-page.component.html',
+  styleUrls: ['./episode-page.component.css']
+})
+export class EpisodePageComponent {
+
+}
