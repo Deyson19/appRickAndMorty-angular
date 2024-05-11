719aea9dd2f9a33a7004b67cf6be88cc1df94095
Deyson Venté <deiandresvent@hotmail.com>
2024-05-10 21:52:59 -0500

    Listado character por id

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CharactersRoutingModule } from './characters-routing.module';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { SharedModule } from '../shared/shared/shared.module';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterPageComponent } from './pages/character-page/character-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CharactersRoutingModule],
+  declarations: [ListPageComponent, CharacterCardComponent, CharacterPageComponent],
+  imports: [CommonModule, CharactersRoutingModule, SharedModule],
 })
 export class CharactersModule {}
