eed68a2f5d255fd1f5d53e84a828622f86d121ff
Deyson Venté <deiandresvent@hotmail.com>
2024-05-21 21:58:17 -0500

    renderiza las rutas y sus componentes

--- src/app/locations/layout/locations-layout/locations-layout.component.ts
+++ src/app/locations/layout/locations-layout/locations-layout.component.ts
@@ -4,9 +4,6 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  template: `
-    <p>locations-layout works!</p>
-    <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class LocationsLayoutComponent {}
