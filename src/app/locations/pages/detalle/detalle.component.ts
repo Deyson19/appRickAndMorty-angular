e1524c5704f1fdf8c73bd9c0276a4bfbc432f8c7
Deyson Venté <deiandresvent@hotmail.com>
2024-05-21 22:20:52 -0500

    detalle realizado

--- src/app/locations/pages/detalle/detalle.component.ts
+++ src/app/locations/pages/detalle/detalle.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LocationsService } from '../../services/locations.service';
+import { Location } from '../../interfaces/locations-response';
 
 @Component({
   templateUrl: './detalle.component.html',
-  styleUrls: ['./detalle.component.css']
+  styleUrls: ['./detalle.component.css'],
 })
-export class DetalleComponent {
+export class DetalleComponent implements OnInit {
+  private _route = inject(Router);
+  private _activeRoute = inject(ActivatedRoute);
+  private _locationsService = inject(LocationsService);
+  isLoading = true;
+  mensaje = 'Buscando datos del lugar'.toUpperCase();
+  location?: Location;
 
+  ngOnInit(): void {
+    const id = this._activeRoute.snapshot.paramMap.get('id');
+    this._locationsService.obtenerPorId(Number.parseInt(id!)).subscribe((x) => {
+      if (x.id > 0) {
+        this.location = x;
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 1200);
+      } else {
+        this._route.navigate(['/locations']);
+      }
+    });
+  }
 }
