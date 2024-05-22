a4228a77bcf17b09a66cbff7ffa1a165d80a7bb1
Deyson Venté <deiandresvent@hotmail.com>
2024-05-21 21:12:19 -0500

    ver detalle del lugar seleccionado

---
+++ src/app/locations/pages/detalle/detalle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.css']
+})
+export class DetalleComponent {
+
+}
